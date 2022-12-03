117dc86b7e9217c0950c4659e8340f817eb6013b
Artūras <arturas008@gmail.com>
2022-12-03 20:56:20 +0200

    feat: classes tasks fully completed

--- 3 savaitė - 12-05/classes-tasks-1.js
+++ 3 savaitė - 12-05/classes-tasks-1.js
@@ -1,3 +1,40 @@
+class Automobilis {
+  constructor(brand, model, price) {
+    this.brand = brand;
+    this.model = model;
+    this.price = price;
+  }
+  raisePriceBy(numb) {
+    return (this.price += numb);
+  }
+}
+const car1 = new Automobilis("Toyota", "Prius", 4500);
+const car2 = new Automobilis("Honda", "Civic", 5700);
+const car3 = new Automobilis("Opel", "Astra", 3800);
+const carArr = [car1, car2, car3];
+console.table(carArr);
+console.log("------------------");
+console.log(car1.model);
+console.log(car2.model);
+console.log(car3.model);
+console.log("------------------");
+console.log(car1.brand);
+console.log(car2.brand);
+console.log(car3.brand);
+console.log("------------------");
+console.log(car1.price);
+console.log(car2.price);
+console.log(car3.price);
+console.log("------------------");
+car1.price = car1.price + 100;
+car2.price = car2.price + 100;
+car3.price = car3.price + 100;
+console.table(carArr);
+console.log("------------------");
+car1.raisePriceBy(100);
+car2.raisePriceBy(500);
+car3.raisePriceBy(1000);
+console.table(carArr);
 /*
 1. Sukurti klasę Automobilis
   - sukurti konstruktorių, su 3 argumentais:
@@ -7,6 +44,7 @@
     - this.brand
     - this.model
     - this.price
+  
 
 2. Sukurkite 3 skirtingus objektus pagal klasę Automobilis kintamuosiuose:
   - const car1 = new Automobilis(..., ..., ...);
